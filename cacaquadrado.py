ee390a58b5f32879006998774e2f4df2de61a562
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2025-03-03 10:06:36 +0000

    Update cacaquadrado.py

--- cacaquadrado.py
+++ cacaquadrado.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random as r
 
+
 score = 0
 countdown = 30
 
@@ -18,9 +19,17 @@ def move1():
         x = r.randint(10, 400)
         y = r.randint(10, 400)
         canvas.coords(sqr, x, y, x+50, y+50)
-        root.after(700, move1)
+        root.after(r.randint(500, 700), move1)
     else:
         canvas.itemconfig(sqr, fill="#000000")
+
+
+def timer():
+    global countdown
+    if not countdown == 0:
+        countdown -= 1
+        timelbl.config(text=f"Tempo: {countdown}")
+        root.after(1000, timer)
 
 
 root = tk.Tk()
@@ -30,6 +39,8 @@ root.wm_resizable(width=False, height=False)
 
 canvas = tk.Canvas(root, width=500, height=500, bg="#FFFFFF")
 canvas.pack()
+
+countdown += 1
 
 sqr = canvas.create_rectangle(50, 50, 100, 100, fill="#FF0000")
 
@@ -41,5 +52,6 @@ timelbl.pack()
 canvas.tag_bind(sqr, "<Button-1>", click1)
 
 move1()
+timer()
 
 root.mainloop()
